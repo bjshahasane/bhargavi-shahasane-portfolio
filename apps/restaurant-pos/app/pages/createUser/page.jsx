73ad1d0591acb18ad9469ecdfadc38c9e39a2fd4
bhargavi <bjshahasane@gmail.com>
2026-04-30 18:16:32 +0530

    added username and password

--- apps/restaurant-pos/app/pages/createUser/page.jsx
+++ apps/restaurant-pos/app/pages/createUser/page.jsx
@@ -12,7 +12,11 @@ const Member = async () => {
             <h1 className="text-center">Create user/Sign Up</h1>
             {/* <p>{session?.user?.name}</p>
             <p>{session?.user?.role}</p> */}
-            
+            <div className="d-flex align-items-center justify-content-center flex-column m-3">
+                <p>U: user1@example.com</p>
+                <p>P: user1@123</p>
+            </div>
+
             <UserForm />
         </div>
     )
